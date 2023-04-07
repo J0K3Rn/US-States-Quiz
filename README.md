4bfb89a3172f6f9a38d670e92b1ffbc7f85ffdd7
J0K3Rn <JosephKeeganRichardson@gmail.com>
2023-04-07 11:33:08 -0500

    Publishing

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # US-States-Quiz
  
+US States quiz game made with the Python Turtle library
+ 
+Features:
+- Guess all 50 states
+- Saves states you couldn't guess to a csv for study
+
+How to run:
+- Download repository
+- Open downloaded repository with a command line interface
+- run `python main.py`
+- Game window will open
+- Type in a correct state name and the name will be marked on the map
+- Type in `exit` to give up and have the rest of the states saved to a csv for you to study
+
+Game Start:
+
+![alt text](https://github.com/J0K3Rn/US-States-Quiz/blob/main/screenshots/game_start.png?raw=true) 
+
+A few states guessed:
+
+![alt text](https://github.com/J0K3Rn/US-States-Quiz/blob/main/screenshots/states_guessed.png?raw=true) 
+
---
+++ main.py
@@ -0,0 +1,53 @@
+import turtle
+import pandas
+
+# Used for getting coordinates on map
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+
+
+def main():
+    screen = turtle.Screen()
+    screen.title("U.S. States Game")
+    image = "blank_states_img.gif"
+    screen.addshape(image)
+    turtle.shape(image)
+    correct = 0
+
+    writer = turtle.Turtle()
+    writer.penup()
+    writer.hideturtle()
+    writer.speed("fastest")
+
+    data = pandas.read_csv("50_states.csv")
+    states = data["state"].to_list()
+    x_cor = data["x"].to_list()
+    y_cor = data["y"].to_list()
+
+    guessed_states = []
+    while len(guessed_states) < 50:
+        answer_state = screen.textinput(title=f"{correct}/50 States Correct", prompt="What's another state's name?")
+
+        if answer_state.title() == "Exit":
+            states_to_learn = []
+            for state in states:
+                if state not in guessed_states:
+                    states_to_learn.append(state)
+            states_to_learn_df = pandas.DataFrame(states_to_learn, columns=['states'])
+            states_to_learn_df.to_csv('states_to_learn.csv')
+            break
+
+        for i, state in enumerate(states):
+            if answer_state.upper() == state.upper():
+                guessed_states.append(state)
+                correct += 1
+                writer.goto(int(x_cor[i]), int(y_cor[i]))
+                writer.write(state)
+
+    # Used for getting coordinates on map
+    # turtle.onscreenclick(get_mouse_click_coor)
+    # turtle.mainloop()
+
+
+if __name__ == "__main__":
+    main()
